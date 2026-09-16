7b2db025f97449b6483c82cc92e70862928dc625
Marco Aurélio <marco.mazanti@cliconsultoria.com.br>
2026-09-16 20:29:25 -0300

    first commit

---
+++ src/main/java/tg/DocVers/DocVersApplication.java
@@ -0,0 +1,13 @@
+package tg.DocVers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocVersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocVersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tg/DocVers/DocVersApplicationTests.java
@@ -0,0 +1,13 @@
+package tg.DocVers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocVersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
