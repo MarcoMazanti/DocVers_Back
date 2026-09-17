4b5f6f585ea165c9a30c965602cf0e1f410fd071
Marco Aurélio <marco.mazanti@cliconsultoria.com.br>
2026-09-16 21:44:45 -0300

    Reestruturação da Entidade

--- src/main/java/tg/DocVers/Entity/Empresa.java
+++ src/main/java/tg/DocVers/Entity/Empresa.java
@@ -13,7 +13,15 @@ public class Empresa {
     private Long id;
     private String nome;
     private String cnpj;
-    private String token;
+    private String senha;
+    private String token; // token gerado de forma aleatória e armazenado em hash, o token puro deve ter no mínimo 25 caracteres
+    private String tokenPrefix; // salva as 5 primeiras letras do token puro
     private SituacaoEmpresa situacao;
-    private Date dataLimiteCadastro;
+    private Date dataContrato;
+    private Date dataLimiteContrato;
+
+    // O token não deve sair do sistema se não for para apresentar ele ao respectivo cliente ao efetuar o login no site
+    // Senha e o token serão armazenados no sistema em hash, se for esquecido o token, o usuário poderá solicitar um novo token
+    // Se ultrapassar o limite de tempo de contrato, a conta fica bloqueada até segundas alterações
+    // Possível funcionalidade: enviar um e-mail para a empresa informando a troca do token via KAFKA
 }
